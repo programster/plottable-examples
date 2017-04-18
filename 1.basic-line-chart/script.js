24356d0d652a04a7c1a43e71e67aac44b28244e1
Programster <sdpagent@gmail.com>
2017-04-18 19:52:54 +0100

    removing unused vars. renaming a variable to make more sense and tie in with next point. Redrawing the plot whenever the page resizes.

--- 1.basic-line-chart/script.js
+++ 1.basic-line-chart/script.js
@@ -4,6 +4,7 @@
  * the advantages of using typescript autocomplete when doing that in netbeans on the page
  * directly.
  */
+/// <reference path="jquery.d.ts" />
 /**
  * Generate a fake set of data to graph which has the provided number of points
  * with a minimum y value of 0, and a maximum possible y value of whatever is provided.
@@ -23,8 +24,6 @@ function generateFakeDataset(numPoints, min, max) {
     return data;
 }
 ;
-var msPerDay = 86400000;
-var lineRenderer;
 var maxXValue = 40;
 var maxYValue = 100;
 var maxXAxisValue = maxXValue * 1.1; // add some buffer room.
@@ -33,20 +32,24 @@ var xScale = new Plottable.Scales.Linear().domain([0, maxXValue]);
 var xAxis = new Plottable.Axes.Numeric(xScale, "bottom");
 var yScale = new Plottable.Scales.Linear().domain([0, maxYAxisValue]);
 var yAxis = new Plottable.Axes.Numeric(yScale, "left");
-// Create a line renderer for rendering our datasets.
-lineRenderer = new Plottable.Plots.Line();
-lineRenderer.x(function (d) { return d.x; }, xScale);
-lineRenderer.y(function (d) { return d.y; }, yScale);
-lineRenderer.attr("stroke", function (d, i, ds) { return ds.metadata().colour; });
+// Create a line plot (lline chart) for our datasets.
+var linePlot = new Plottable.Plots.Line();
+linePlot.x(function (d) { return d.x; }, xScale);
+linePlot.y(function (d) { return d.y; }, yScale);
+linePlot.attr("stroke", function (d, i, ds) { return ds.metadata().colour; });
 var datasets = [
     new Plottable.Dataset(generateFakeDataset(maxXValue, 30, maxYValue), { "colour": "#ffa500" }),
     new Plottable.Dataset(generateFakeDataset(maxXValue, 10, 30), { "colour": "green" }),
     new Plottable.Dataset(generateFakeDataset(maxXValue, 0, 10), { "colour": "rgb(0,0,0)" })
 ];
-lineRenderer.datasets(datasets);
+linePlot.datasets(datasets);
 var areaChart = new Plottable.Components.Table([
     [null, null],
-    [yAxis, lineRenderer],
+    [yAxis, linePlot],
     [null, xAxis]
 ]);
 areaChart.renderTo("svg#example");
+// Resize the plot to fit the window on resize.
+$(window).resize(function () {
+    linePlot.redraw();
+});
